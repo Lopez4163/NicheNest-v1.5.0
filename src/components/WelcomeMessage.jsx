be19e0a35f9b145a02765747e1c2c3688983da89
Nicholas Lopez <64043616+Lopez4163@users.noreply.github.com>
2024-09-20 11:53:34 -0400

    added welcome message

--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -28,7 +28,7 @@ const WelcomeMessage = () => {
         <img
           src={logo}
           alt="Bird Logo"
-          className="mx-auto mb-4 h-20 w-20"
+          className="mx-auto mb-4 h-60 w-72"
         />
         <h2 className="text-2xl font-bold mb-4">Welcome to Niche Nest!</h2>
         <p className="mb-4 text-lg">
